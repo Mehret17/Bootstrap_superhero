2a046f2322b9ff221efff6dd774498d9047bd5ca
Mehret Kulala <merutaye@gmail.com>
2018-04-03 20:05:22 -0500

    added dropdown for jobs

--- javascript/jobs.js
+++ javascript/jobs.js
@@ -1,1 +1,40 @@
 console.log ('hi!');
+console.log ('hi!');
+
+const printToDom = (domString, divId) => {
+    document.getElementById(divId).innerHTML = domString;
+};
+
+const domString = (superheroesArray) => {
+    let domString = "";
+    superheroesArray.forEach((superheroes) =>{
+        //domString +=    `<h1></h1>`;  
+        
+        domString += `<li>`;
+        domString += `<a href ='#' data-hero-id="${superheroes.id}">${superheroes.name}</a>`;
+        domString +=  `</li>`;
+        
+    });
+    printToDom (domString, 'awesome-dropdown');
+};
+
+function executeThisFunctionAfterFileLoads () {
+    const data = JSON.parse(this.responseText);
+    console.log ("data",data);
+    domString (data.superheroes);
+}
+
+function WIW () {
+    console.log("something went wrong");
+}
+
+const startApplication = () => {
+    let myRequest = new XMLHttpRequest ();
+    myRequest.addEventListener("load",executeThisFunctionAfterFileLoads);
+    myRequest.addEventListener("error",WIW);
+    myRequest.open("GET","/db/superheroes.json");
+    myRequest.send();
+    console.log ("myrequest", myRequest);
+}
+
+startApplication ();
